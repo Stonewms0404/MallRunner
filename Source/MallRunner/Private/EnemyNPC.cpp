0ff7aef9aede2b86b29da7f998ab28f3d4e05c29
Stonewms0404 <48721922+Stonewms0404@users.noreply.github.com>
2024-06-12 11:21:14 -0400

    Base Outline For Enemies

--- Source/MallRunner/Private/EnemyNPC.cpp
+++ Source/MallRunner/Private/EnemyNPC.cpp
@@ -1,5 +1,84 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "EnemyNPC.h"
 
+void AEnemyNPC::BeginPlay() {
+	Super::BeginPlay();
+
+	controller = Cast<AEnemyController>(GetController());
+}
+
+void AEnemyNPC::Tick(float deltaTime)
+{
+	Super::Tick(deltaTime);
+	charMoveComp->MaxWalkSpeed = isRunning ? (inCrowd ? stats.sprintSpeed / 1.5 : stats.sprintSpeed) : (inCrowd ? stats.walkSpeed / 1.5 : stats.walkSpeed);
+
+	switch (state) {
+	case EEnemyState::VE_Idle:
+		if (!idleTimerStarted) {
+			StartIdleTimer();
+			break;
+		}
+
+		if (UseIdleTimer(deltaTime))
+			state = EEnemyState::VE_Patrolling;
+		break;
+
+	case EEnemyState::VE_Patrolling:
+		if (atPatrolLocation) {
+			GetPatrolLocation();
+
+			state = EEnemyState::VE_Idle;
+			break;
+		}
+
+		controller->MoveToLocation(patrolLocation);
+		
+		CheckPatrolLocation();
+		break;
+
+	case EEnemyState::VE_Searching:
+		if (!searchTimerStarted) {
+			StartSearchTimer();
+			break;
+		}
+
+		if (UseSearchTimer(deltaTime)) {
+			state = EEnemyState::VE_Idle;
+			break;
+		}
+
+		controller->MoveToLocation(lastPlayerLocation);
+		break;
+
+	case EEnemyState::VE_Chasing:
+		if (isFrozen) {
+
+			break;
+		}
+
+		if (!canSeePlayer) {
+			state = EEnemyState::VE_Searching;
+			break;
+		}
+
+		controller->MoveToActor(playerController->PlayerCharacter, -1, false);
+		break;
+	}
+}
+
+void AEnemyNPC::StartSearchTimer() {
+	searchTimer = 0;
+	searchTimerLength = rand() % 10;
+}
+
+bool AEnemyNPC::UseSearchTimer(float deltaTime) {
+	searchTimer += deltaTime;
+
+	return idleTimer >= searchTimerLength;
+}
+
+void AEnemyNPC::GetPatrolLocation() {
+	FVector playerPos = playerController->PlayerCharacter->GetActorLocation();
+	patrolLocation = playerPos + FVector((rand() % 200) - 100, (rand() % 200) - 100, playerPos.Z);
+}
--- Source/MallRunner/Public/BaseAIController.h
+++ Source/MallRunner/Public/BaseAIController.h
@@ -1,17 +1,66 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "AIController.h"
 #include "BaseAIController.generated.h"
 
-/**
- * 
- */
+USTRUCT(BlueprintType)
+struct FNPCStruct {
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NPC|Stats|Variables", meta = (ToolTip = "MaxWalkSpeed of the Character Movement Component for sprinting"))
+	float sprintSpeed;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NPC|Stats|Variables", meta = (ToolTip = "MaxWalkSpeed of the Character Movement Component for walking"))
+	float walkSpeed;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NPC|Stats|Variables", meta = (ToolTip = "MaxAcceleration of the Character Movement Component\n  The player will change it's velocity every second by this value"))
+	float accelerationSpeed;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NPC|Stats|Variables", meta = (ToolTip = "GroundFriction of the Character Movement Component\n  The player will use this friction on the ground to move\n    Max of 8"))
+	float traction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NPC|Stats|Variables", meta = (ToolTip = "BrakingFriction of the Character Movement Component\n  The player will use this friction on the ground to move\n    Max of 1"))
+	float handling;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NPC|Stats|Variables", meta = (ToolTip = "Normal walk height of the player"))
+	float normalHeight;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NPC|Stats|Variables", meta = (ToolTip = "Normal walk height of the player"))
+	float forgetTimer;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NPC|Stats|Variables", meta = (ToolTip = "The scale of gravity that the player will fall"))
+	float gravity;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "NPC|Stats|Variables", meta = (ToolTip = "The player's weight and how easily it is for a the player to break \"Breakable\" Objects"))
+	int weight;
+
+	UPROPERTY(BlueprintReadOnly, Category = "NPC|Variables")
+	bool canAlert;
+	UPROPERTY(BlueprintReadOnly, Category = "NPC|Variables")
+	bool canCatch;
+};
+
+UENUM(BlueprintType)
+enum class ENPCType : uint8 {
+	VE_Light = 0		UMETA(DisplayName = "Light"),
+	VE_Normal = 1		UMETA(DisplayName = "Normal"),
+	VE_Heavy = 2		UMETA(DisplayName = "Heavy")
+};
+
 UCLASS()
 class MALLRUNNER_API ABaseAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "NPC|Enemy|Variables")
+	bool canAlert;
+	UPROPERTY(EditDefaultsOnly, Category = "NPC|Enemy|Variables")
+	bool canCatch;
+	UPROPERTY(EditDefaultsOnly, Category = "NPC|Variables")
+	ENPCType npcType;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "NPC|Stats")
+	FNPCStruct LightEnemy;
+	UPROPERTY(EditDefaultsOnly, Category = "NPC|Stats")
+	FNPCStruct NormalEnemy;
+	UPROPERTY(EditDefaultsOnly, Category = "NPC|Stats")
+	FNPCStruct HeavyEnemy;
+
+	virtual void OnPossess(APawn* aPawn) override;
+	virtual void OnUnPossess() override;
 };
